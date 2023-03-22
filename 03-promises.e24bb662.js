077fb0202e6eda84ac8ed557dc765c9531b320e0
Iryna-Bigdash <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-03-22 15:39:12 +0000

    Deploying to gh-pages from @ Iryna-Bigdash/goit-js-hw-09@4e76095b990102213f98458bfa43939839271a52 🚀

--- 03-promises.e24bb662.js
+++
@@ -1,2 +0,0 @@
-const t={sbtBtn:document.querySelector("button")};console.log(t.sbtBtn);
-//# sourceMappingURL=03-promises.e24bb662.js.map
--- 03-promises.fd4f641f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={sbtBtn:document.querySelector("button")};console.log(t.sbtBtn)}();
-//# sourceMappingURL=03-promises.fd4f641f.js.map
